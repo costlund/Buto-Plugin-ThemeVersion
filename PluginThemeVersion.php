d39c4058a7f67ea1262d3320d4f27e0ab548a75c
Claes Östlund <claesostlund@Mefromhd-MBP18.local>
2018-12-03 13:08:19 +0100

    Error handling when file not exist or param history not exist.

--- PluginThemeVersion.php
+++ PluginThemeVersion.php
@@ -10,44 +10,56 @@ class PluginThemeVersion{
   }
   public function widget_history($data){
     $data = new PluginWfArray($data);
-    $data = new PluginWfYml($data->get('data/filename'));
-    $history = new PluginWfArray($data->get('history'));
+    if(!wfFilesystem::fileExist(wfGlobals::getAppDir().$data->get('data/filename'))){
+      throw new Exception("PluginThemeVersion.widget_history says: File ".$data->get('data/filename')." does not exist.");
+    }
+    $yml = new PluginWfYml($data->get('data/filename'));
+    $history = new PluginWfArray($yml->get('history'));
     /**
      * Data fix.
      */
-    foreach ($history->get() as $key => $value) {
-      $item = new PluginWfArray($value);
-      if(wfUser::hasRole('webmaster') && $item->get('webmaster')){
-        $history->set("$key/webmaster_enabled", true);
-      }else{
-        $history->set("$key/webmaster_enabled", false);
+    if($history->get()){
+      foreach ($history->get() as $key => $value) {
+        $item = new PluginWfArray($value);
+        if(wfUser::hasRole('webmaster') && $item->get('webmaster')){
+          $history->set("$key/webmaster_enabled", true);
+        }else{
+          $history->set("$key/webmaster_enabled", false);
+        }
+        $history->set("$key/version", $key);
       }
-      $history->set("$key/version", $key);
-    }
-    /**
-     * New key to sort on.
-     */
-    $temp = array();
-    foreach ($history->get() as $key => $value) {
-      $a = preg_split('/[.]/', $key);
-      $new_key = null;
-      foreach ($a as $value2) {
-        $new_key .= ($value2+1000);
+      /**
+       * New key to sort on.
+       */
+      $temp = array();
+      foreach ($history->get() as $key => $value) {
+        $a = preg_split('/[.]/', $key);
+        $new_key = null;
+        foreach ($a as $value2) {
+          $new_key .= ($value2+1000);
+        }
+        if(sizeof($a)==2){
+          $new_key .= 1000;
+        }
+        $temp[$new_key] = $value;
       }
-      if(sizeof($a)==2){
-        $new_key .= 1000;
+      krsort($temp);
+      $history = new PluginWfArray($temp);
+      /**
+       * Render elements.
+       */
+      foreach ($history->get() as $key => $value){
+        $item = new PluginWfArray($value);
+        $element = new pluginwfyml('/plugin/theme/version/element/history_item.yml');
+        $element->setByTag($item->get());
+        wfDocument::renderElement($element->get());
       }
-      $temp[$new_key] = $value;
-    }
-    krsort($temp);
-    $history = new PluginWfArray($temp);
-    /**
-     * Render elements.
-     */
-    foreach ($history->get() as $key => $value){
-      $item = new PluginWfArray($value);
-      $element = new pluginwfyml('/plugin/theme/version/element/history_item.yml');
-      $element->setByTag($item->get());
+    }else{
+      /**
+       * If no data.
+       */
+      $element = new pluginwfyml('/plugin/theme/version/element/history_missing.yml');
+      $element->setByTag($data->get('data'));
       wfDocument::renderElement($element->get());
     }
   }
